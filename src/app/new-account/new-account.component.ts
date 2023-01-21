4061fb4abd26d8dcf093688b78054cd40e1c9c43
vinayak <vnkakmt@gmail.com>
2023-01-21 18:14:09 +0530

    Hierarchical injector example

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,23 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../account.service';
 import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers:[LoggingService]
+  providers:[LoggingService]  //due to hierarchical injector concept. We dont have to use accountService since its available in app.component
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  constructor(private loggingService:LoggingService, 
+              private accountService:AccountsService){
 
-  constructor(private loggingService:LoggingService){}
+              }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
+    this.accountService.addAccount(accountName, accountStatus)
     this.loggingService.logStatusChange(accountStatus);
    // console.log('A server status changed, new status: ' + accountStatus);
   }
